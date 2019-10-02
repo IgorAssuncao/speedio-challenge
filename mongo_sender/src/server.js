79c99781e3cd69866cda56967670745254792a53
IgorAssuncao <igor.ormonde@gmail.com>
2019-10-02 20:20:35 -0300

    initialized all projects

---
+++ mongo_sender/src/server.js
@@ -0,0 +1,13 @@
+import app from "./app";
+
+const host = "0.0.0.0";
+const port = 3001;
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : "local";
+
+app.start = (HOST, PORT) => {
+  return app.listen(PORT, HOST, () =>
+    console.log(`${env} Server - API Running on ${HOST}:${PORT}`)
+  );
+};
+
+app.start(host, port);
